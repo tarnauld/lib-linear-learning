69332eb5b5820ec4e589c411ed15a7eb7ab9804d
Aurelien Duval <aurelien.duval6@gmail.com>
2018-03-07 17:46:37 +0100

    Corrected main issues

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ extern crate nalgebra;
 use nalgebra::core::dimension::Dynamic as Dynamic;
 use nalgebra::core::MatrixMN as MatrixMN;
 
+use rand::{Rng};
+
 struct Point {
     x: f64,
     y: f64,
@@ -23,30 +25,21 @@ fn convert_to_raw_data(point: &Point) -> [f64; 3]{
 }
 
 #[no_mangle]
-pub extern fn classify(row : [f64; 2], w: [f64; 3]) -> i32{
-	let mut activation = w[0];
-
-	activation += w[0] * row[0];
-    activation += w[1] * row[1];
-    activation += w[2];
-
-	if activation >= 0.0 {
-		return 1;
-	}
-	return -1;
+pub unsafe extern fn classify(row :*mut [f64; 2], w:*mut [f64; 3]) -> f64{
+    let point = convert_to_point(&[(*row)[0], 0.0, (*row)[1]]);
+	predict(&point, *w)
 }
 
-fn predict(row : &Point, w: [f64; 3]) -> i32{
+fn predict(row : &Point, w: [f64; 3]) -> f64{
 	let mut activation = w[0];
 
-	activation += w[0] * row.x;
-    activation += w[1] * row.y;
+	activation += w[1] * row.x;
     activation += w[2] * row.z;
 
 	if activation >= 0.0 {
-		return 1;
+		return 1.0;
 	}
-	return -1;
+	return -1.0;
 }
 
 fn export_external(w: &[f64]) -> *mut std::os::raw::c_void{
@@ -60,32 +53,37 @@ fn import_external<'a>(array: *mut std::os::raw::c_void, length : u32) -> &'a[f6
 }
 
 #[no_mangle]
-pub extern fn weights_training(weights: *mut[f64; 3], data_set: [f64; 9]){
-    let nb = 5;
-    let step = 0.1;
+pub extern fn weights_training(weights: *mut[f64; 3], data_set: *mut [f64; 9]) -> *mut[f64; 3]{
+    let nb = 10000000;
+    let step:f64 = 0.01;
 
     let mut points : Vec<Point> = Vec::new();
     let mut i = 0;
-    loop{
-        if i == 9{break;}
-        let mut tmp : [f64;3] = [data_set[i as usize], data_set[(i + 1) as usize], data_set[(i + 2) as usize]];
-        points.push(convert_to_point(&tmp));
-        i += 3;
+    unsafe{
+        loop{
+            if i == 9{break;}
+            let mut tmp : [f64;3] = [(*data_set)[i as usize], (*data_set)[(i + 1) as usize], (*data_set)[(i + 2) as usize]];
+            points.push(convert_to_point(&tmp));
+            i += 3;
+        }
     }
 
-    for _i in 0..nb{
-        for (_i, point) in points.iter().enumerate() {
+    for i in 0..nb{
+        for (j, point) in points.iter().enumerate() {
             let row = convert_to_raw_data(point);
             unsafe{
                 let prediction = predict(point, *weights);
-                let error = row[2] - prediction as f64;
+                let error = (row[1] - prediction) as f64;
                 (*weights)[0] = (*weights)[0] + step * error;
-                for i in 0..row.len() - 1{
-                    (*weights)[i + 1] = (*weights)[i + 1] + step * error * row[i];
-                }
+                (*weights)[1] = (*weights)[1] + step * error * row[0];
+                (*weights)[2] = (*weights)[2] + step * error * row[2];
+                // for k in 0..row.len() - 1{
+                //     (*weights)[k + 1] = (*weights)[k + 1] + step * error * row[k];
+                // }
             }
         }
     }
+    weights
 }
 
 #[no_mangle]
@@ -93,8 +91,7 @@ pub unsafe extern "C" fn generate_weight() -> *mut[f64; 3]{
 	let mut w: [f64; 3] = [0.0; 3];
 
     for i in 0..3{
-        let x = rand::random::<f64>();
-        w[i as usize] = (x * 2.0) - 1.0;
+        w[i as usize] = rand::thread_rng().gen_range(-1., 1.);
     }
    	Box::into_raw(Box::new(w))
 }
